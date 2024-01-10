dc27e9b83ebd3aec0cc224c5befeaeda9a8f8ea9
ra <118706048+rauliaa@users.noreply.github.com>
2024-01-10 20:24:01 +0700

    header

--- index.php
+++ index.php
@@ -4,23 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Media Online</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <h1>Media Online</h1>
-    <h2>Sosial Media Developer</h2>
-    <p>Belajar dan berbagi agar hidup menjadi lebih baik </p>
-    
-    <h2>Benefit Join di Media Online</h2>
-    <ul>
-    <li>Mendapatkan motivasi dari sesama para developer</li>
-    <li>Sharing knowledge</li>
-    <li>Dibuat oleh calon web developer terbaik</li>
-</ul>
+    <header id="header">
+        <div class="container">
+            <h1>Media Online</h1>
+            <h2>Sosial Media Developer</h2>
+            <p>Belajar dan berbagi agar hidup menjadi lebih baik </p>
+
+            <nav id="navbar" class="navbar">
+                
+            </nav>
+    </header>
+            <h2>Benefit Join di Media Online</h2>
+            <ul>
+            <li>Mendapatkan motivasi dari sesama para developer</li>
+            <li>Sharing knowledge</li>
+            <li>Dibuat oleh calon web developer terbaik</li>
+        </ul>
     
 <h2>Cara Bergabung ke Media Online</h2>
     <ol>
         <li>Mengunjungi website ini</li>
-        <li>Mendaftarkan di <a href="soal2.php">Form Sign Up</a></li>
+        <li>Mendaftarkan di <a href="form.php">Form Sign Up</a></li>
         <li>Selesai</li>
         </ol>
 </body>
