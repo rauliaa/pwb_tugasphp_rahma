4afb9cbfb1ad066505481835690cccf4b540aa07
ra <118706048+rauliaa@users.noreply.github.com>
2024-01-11 19:12:11 +0700

    memperbarui penempatan

--- index.php
+++ index.php
@@ -3,66 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulir</title>
+    <title>Media Online</title>
 </head>
 <body>
-    <form method="post" action="welcome.php" onsubmit="return validateForm()">
-        <h1>Buat Akun Baru</h1>
-        <h2>Formulir Pendaftaran</h2>
-
-        <label for="firstname">Nama Depan :</label><br><br>
-        <input type="text" name="firstname" id="firstname"><br><br>
-
-        <label for="lastname">Nama Belakang :</label><br><br>
-        <input type="text" name="lastname" id="lastname"><br><br>
-
-        <label for="Gender">Jenis Kelamin</label><br><br>
-        <input type="radio" name="gender" value="Male" id="male">Pria
-        <input type="radio" name="gender" value="Female" id="female">Wanita<br><br>
-
-        <label for="Nationality">Kewarganegaraan</label><br><br> 
-        <select name="nationality" id="nationality">
-            <option value="Indonesia">Indonesia</option>
-            <option value="Singapore">Singapore</option>
-            <option value="China">China</option>
-            <option value="Korea Selatan">Korea Selatan</option>
-        </select><br><br>
-
-        <label for="Language Spoken">Bahasa yang Dikuasai</label><br><br>
-        <input type="checkbox" name="languages[]" value="Bahasa Indonesia">Bahasa Indonesia<br>
-        <input type="checkbox" name="languages[]" value="Bahasa Inggris">Bahasa Inggris<br>
-        <input type="checkbox" name="languages[]" value="Bahasa Mandarin">Bahasa Mandarin<br>
-        <input type="checkbox" name="languages[]" value="Bahasa Korea">Bahasa Korea<br>
-        <input type="checkbox" name="languages[]" value="Lainnya">Lainnya<br><br>
-
-        <label for="bio">Bio</label><br>
-        <textarea name="bio" id="bio" cols="30" rows="10"></textarea><br><br>
-        
-        <button type="submit" onclick="return validateForm()">Daftar</button>
-        <p class="warning" id="warning"></p>
-
-        <script>
-            function validateForm() {
-                var firstname = document.getElementById('firstname').value;
-                var lastname = document.getElementById('lastname').value;
-                var nationality = document.getElementById('nationality').value;
-                var bio = document.getElementById('bio').value;
-
-                var maleChecked = document.getElementById('male').checked;
-                var femaleChecked = document.getElementById('female').checked;
-
-                var warningElement = document.getElementById('warning');
-
-                if (firstname === "" || lastname === "" || nationality === "" || bio === "" || (!maleChecked && !femaleChecked)) {
-                    warningElement.innerHTML = "Harap isi semua kolom yang diperlukan!";
-                    return false;
-                } else {
-                    warningElement.innerHTML = "";
-                    return true;
-                }
-            }
-        </script>
-
-    </form>
+    <h1>Media Online</h1>
+    <h2>Sosial Media Developer</h2>
+    <p>Belajar dan berbagi agar hidup menjadi lebih baik </p>
+    
+    <h2>Benefit Join di Media Online</h2>
+    <ul>
+    <li>Mendapatkan motivasi dari sesama para developer</li>
+    <li>Sharing knowledge</li>
+    <li>Dibuat oleh calon web developer terbaik</li>
+</ul>
+    
+<h2>Cara Bergabung ke Media Online</h2>
+    <ol>
+        <li>Mengunjungi website ini</li>
+        <li>Mendaftarkan di <a href="form.php">Form Sign Up</a></li>
+        <li>Selesai</li>
+        </ol>
 </body>
 </html>
